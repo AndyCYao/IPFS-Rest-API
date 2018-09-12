35fff64ac136b2a398f555dd27681052ffc81772
Andy Y <andyyao3@gmail.com>
2018-09-11 22:10:18 -0700

    + rename app to rest

--- rest/run.py
+++ rest/run.py
@@ -1,5 +1,5 @@
 from app import app
 # I created run.py as runner because there were circular reference issues
-# w.r.t to python imports
+# w.r.t to python imports if we just use app.py
 # https://stackoverflow.com/questions/21766960/operationalerror-no-such-table-in-flask-with-sqlalchemy
 app.run(host='0.0.0.0', port=8081, debug=True)
