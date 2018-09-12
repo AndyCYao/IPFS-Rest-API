c37680e9b587f87838e071a966a0652603d88d4a
Andy Y <andyyao3@gmail.com>
2018-09-11 21:02:13 -0700

    * Refactored add and delete endpoint into flask-restful

--- app/app.py
+++ app/app.py
@@ -5,9 +5,6 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
-IPFS_BASE_URL = "http://localhost:5001"
-IPFS_API_VER  = "api/v0"
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -37,75 +34,15 @@ api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
 api.add_resource(resources.TokenRefresh, '/token/refresh')
 api.add_resource(resources.AllUsers, '/users')
 api.add_resource(resources.SecretResource, '/secret')
+api.add_resource(resources.AddFile, '/add')
+api.add_resource(resources.DeleteFile, '/delete')
 
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
     return models.RevokedTokenModel.is_jti_blacklisted(jti)
 
+
 @app.route('/')
 def index():
     return 'Hello'
-
-
-@app.route("/add", methods=['POST'])
-def add():
-    '''connects to /api/v0/add
-    args [file] required
-    '''
-    data        = flask_request.get_json()
-    sourceFile  = data["files"]
-    files = {'files': (sourceFile, open(sourceFile, 'rb'))}
-
-    response = requests.post('{}/{}/add'.format(IPFS_BASE_URL, IPFS_API_VER),
-                                        files=files)
-    print(response)
-    print(response.json())
-    if(response.status_code == 200):
-        responseJson = response.json()
-        responseHash = responseJson["Hash"]
-        # Response from IPFS
-        payload = {
-            "success": True,
-            "error": 0,
-            "fileUrl": "{}/{}".format(IPFS_BASE_URL, responseHash),
-            "fileHash": responseHash
-        }
-        return Response(json.dumps(payload), status=200,
-                        mimetype='application/json')
-    else:
-        return abort(500)
-
-
-def _garbageCollect():
-    '''performs garbage colection
-    '''
-    response = requests.post('{}/{}/repo/gc'.format(IPFS_BASE_URL, IPFS_API_VER))
-    return response.status_code
-
-
-@app.route("/delete", methods=['POST'])
-def delete():
-    '''connects to /api/v0/pin/rm
-    args [files] required
-    since IPFS is permanent, this will unpin the file and call garbage collection
-    '''
-    data  = flask_request.get_json()
-    file  = data["files"]
-    response = requests.post('{}/{}/pin/rm?arg={}'.format(IPFS_BASE_URL, IPFS_API_VER, file))
-    print(response.status_code)
-    print(response.json())
-    if response.status_code == 200 and _garbageCollect() == 200:
-        payload = {
-            "success": True,
-            "error": ""
-        }
-        return Response(json.dumps(payload), status=200, 
-                        mimetype="application/json")
-    else:
-        return abort(500)
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    return json.dumps({"error": "Error Handling Data"})
--- app/resources.py
+++ app/resources.py
@@ -4,14 +4,21 @@ from flask_jwt_extended import (create_access_token,
                                 create_refresh_token, jwt_required,
                                 jwt_refresh_token_required, get_jwt_identity,
                                 get_raw_jwt)
-parser = reqparse.RequestParser()
-parser.add_argument('username', help='This field cannot be blank', required=True)
-parser.add_argument('password', help='This field cannot be blank', required=True)
+import requests
+
+
+IPFS_BASE_URL = "http://localhost:5001"
+IPFS_API_VER  = "api/v0"
 
 
 class UserRegistration(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', help='This field cannot be blank', required=True)
+    parser.add_argument('password', help='This field cannot be blank', required=True)
+
     def post(self):
-        data = parser.parse_args()
+        data = UserRegistration.parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {'message': 'User {} already exists'.format(data['username'])}
         new_user = UserModel(
@@ -32,8 +39,13 @@ class UserRegistration(Resource):
 
 
 class UserLogin(Resource):
+    
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', help='This field cannot be blank', required=True)
+    parser.add_argument('password', help='This field cannot be blank', required=True)
+
     def post(self):
-        data = parser.parse_args()
+        data = UserRegistration.parser.parse_args()
         current_user = UserModel.find_by_username(data['username'])
         if not current_user:
             return {'message': 'User {} doesn\'t exist'.format(data['username'])}
@@ -65,7 +77,7 @@ class UserLogoutRefresh(Resource):
     def post(self):
         jti = get_raw_jwt()['jti']
         try:
-            revoked_token = RevokedTokenModel(jti = jti)
+            revoked_token = RevokedTokenModel(jti=jti)
             revoked_token.add()
             return {'message': 'Refresh token has been revoked'}
         except:
@@ -92,3 +104,66 @@ class SecretResource(Resource):
     @jwt_required
     def get(self):
         return {'answer': 42}
+
+
+class AddFile(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('fileName', help='Required, File Name', required=True)
+
+    @jwt_required
+    def post(self):
+        '''connects to /api/v0/add
+        args [file] required
+        '''
+        data = AddFile.parser.parse_args()
+        files = {'files': (data['fileName'], open(data["fileName"], 'rb'))}
+
+        response = requests.post('{}/{}/add'.format(IPFS_BASE_URL, IPFS_API_VER),
+                                files=files)
+        print(response)
+        print(response.json())
+        if(response.status_code == 200):
+            responseJson = response.json()
+            responseHash = responseJson["Hash"]
+            # Response from IPFS
+            return {
+                'success': True,
+                'error': 0,
+                'fileUrl': "{}/{}".format(IPFS_BASE_URL, responseHash),
+                'fileHash': responseHash
+            }, 200
+        else:
+            return {'message': 'could not add file'}, 500
+
+
+class DeleteFile(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('file', help='Required, Content Address Of the File', required=True)
+
+    @jwt_required
+    def post(self):
+        '''connects to /api/v0/pin/rm
+        args [file] required
+        since IPFS is permanent, this will unpin the file and call garbage collection
+        '''
+        # data  = flask_request.get_json()
+        data = DeleteFile.parser.parse_args()
+        response = requests.post('{}/{}/pin/rm?arg={}'.format(IPFS_BASE_URL, IPFS_API_VER, data['file']))
+        print(response.status_code)
+        print(response.json())
+        if response.status_code == 200 and self.garbageCollect() == 200:
+            return {
+                'success': True,
+                'error': ''
+            }, 200
+        else:
+            return {'message': 'could not delete file'}, 500
+        
+    def garbageCollect(self):
+        '''performs garbage colection
+        '''
+        response = requests.post('{}/{}/repo/gc'.format(IPFS_BASE_URL, IPFS_API_VER))
+        return response.status_code
+
